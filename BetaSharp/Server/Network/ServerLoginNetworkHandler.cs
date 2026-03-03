0d912120aa8b3391ecf60ff8d64f7f17fbca4ea2
TheVeryStarlk <45287860+TheVeryStarlk@users.noreply.github.com>
2026-03-04 02:47:06 +0300

    Fix

--- BetaSharp/Server/Network/ServerLoginNetworkHandler.cs
+++ BetaSharp/Server/Network/ServerLoginNetworkHandler.cs
@@ -139,7 +139,7 @@ public class ServerLoginNetworkHandler : NetHandler
             handler.sendPacket(new LoginHelloPacket("", ent.id, var3.getSeed(), (sbyte)var3.dimension.Id));
             handler.sendPacket(new PlayerSpawnPositionS2CPacket(var4.X, var4.Y, var4.Z));
             server.playerManager.sendWorldInfo(ent, var3);
-            Discord.Channel!.SendMessageAsync($"{ent.name} join the game.");
+            Discord.Channel!.SendMessageAsync($"{ent.name} joined the game.");
             server.playerManager.sendToAll(new ChatMessagePacket("§e" + ent.name + " joined the game."));
             server.playerManager.addPlayer(ent);
             handler.teleport(ent.x, ent.y, ent.z, ent.yaw, ent.pitch);
