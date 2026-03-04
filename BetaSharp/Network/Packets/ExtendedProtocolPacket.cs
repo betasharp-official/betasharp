9becc8edea17a4a46975693e776dd6f2d9122697
one-some <onesomegit@outlook.com>
2026-03-04 17:36:15 -0600

    Cleanup

--- BetaSharp/Network/Packets/ExtendedProtocolPacket.cs
+++ BetaSharp/Network/Packets/ExtendedProtocolPacket.cs
@@ -6,14 +6,4 @@ public abstract class ExtendedProtocolPacket : Packet
 {
     public ExtendedProtocolPacket(byte id) : base(id) { }
     public ExtendedProtocolPacket(PacketId id) : base(id) { }
-
-    public override void Read(NetworkStream stream)
-    {
-        //EntityId = stream.ReadInt();
-    }
-
-    public override void Write(NetworkStream stream)
-    {
-        // stream.WriteInt(EntityId);
-    }
 }
